dc73bfc73a1ee83600a90853230a8def0a48249f
Los Polos <los.polos2015@gmail.com>
2017-01-15 11:35:42 +0300

    three commit

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
         if(!w.snake.checkAlive())
             break;
     }while(true);
-
+    
+    // изменения в файле для создания нового коммита)
     return 0;
 }
